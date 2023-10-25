7c2118e427a120914e56ca0044a554b2b5e5d0c4
Brandon Duong <bduong1497@gmail.com>
2023-10-25 17:25:23 -0400

    Added button functionality for displaying/hiding details

--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const API_IMG = "https://image.tmdb.org/t/p/w500/";
-const MovieBox = ({
-  title,
-  poster_path,
-  vote_average,
-  release_date,
-  overview,
-  popularity,
-}) => {
+
+const MovieBox = ({ title, poster_path, release_date, overview }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
   return (
     <div className="card-container">
       <img src={API_IMG + poster_path} alt={title}></img>
       <h1>{title}</h1>
-      <p>{overview}</p>
-      <span>{release_date}</span>
+
+      {showDetails ? <p>{overview}</p> : null}
+      <button onClick={toggleDetails}>
+        {showDetails ? "Hide Details" : "Show Details"}
+      </button>
+
+      <div>{release_date}</div>
     </div>
   );
 };
